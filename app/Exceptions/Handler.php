d12071c37d79badadfa953d80804f9faa655c695
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-09-25 15:31:11 +0100

    wip

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,7 +18,7 @@ class Handler extends ExceptionHandler
     {
         $response = parent::render($request, $e);
 
-        if (! app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403,409])) {
+        if (! app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403,409,405])) {
             return Inertia::render('Error', ['status' => $response->status()])
                 ->toResponse($request)
                 ->setStatusCode($response->status());
--- resources/js/Pages/Error.vue
+++ resources/js/Pages/Error.vue
@@ -12,6 +12,7 @@ const title = computed(() => {
         404: '404: Page Not Found',
         403: '403: Forbidden',
         409: '409: You\'re already subscribed!',
+        405 :'405: Method Not Allowed',
     }[props.status]
 })
 
@@ -22,6 +23,7 @@ const description = computed(() => {
         404: 'Sorry, the page you are looking for could not be found.',
         403: 'Sorry, you are forbidden from accessing this page.',
         409: 'It looks like you\'re already subscribed to our service. No need to worry, you\'re all set to enjoy our updates and offers. If you have any questions or need assistance, feel free to reach out to our support team. Thank you for being a valued member of our community!',
+        405: 'Please let us know what you were doing when this error occurred. We will fix it as soon as possible. Sorry for any inconvenience caused.',
     }[props.status]
 })
 </script>
