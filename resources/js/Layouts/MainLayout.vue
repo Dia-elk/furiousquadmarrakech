b0fb6cb18b855cb5e37da7a9b896fbd7560dea5c
Dia <macbook@MacBook-Pro-de-macbook.local>
2024-01-21 22:40:37 +0100

    whatsApp

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -5,9 +5,10 @@
     <main class="bg-[#050403]">
         <slot/>
     </main>
-    <footer class="h-[100px] flex flex-col-reverse  justify-center  items-center px-2 md:p-10 border-t-[0.6px] border-gray-600 mx-6 lg:mx-auto lg:max-w-7xl">
+    <footer
+        class="h-[100px] flex flex-col-reverse  justify-center  items-center px-2 md:p-10 border-t-[0.6px] border-gray-600 mx-6 lg:mx-auto lg:max-w-7xl">
         <div>
-            <span class="text-gray-500 lg:text-[12px] text-[10px]">© {{date.getFullYear()}} Furious Quad Marrakech - Made with <span
+            <span class="text-gray-500 lg:text-[12px] text-[10px]">© {{ date.getFullYear() }} Furious Quad Marrakech - Made with <span
                 class="text-[#c10c10]">♥</span> by </span>
             <span class="text-gray-200 lg:text-[12px] text-[8px] font-bold underline cursor-pointer">
                 <a href="https://diaelk.dev/" target="_blank">Dia</a>
@@ -26,11 +27,19 @@
             <a target="_blank" href="https://www.airbnb.fr/users/show/543464602">
                 <Airbnb/>
             </a>
-            <a target="_blank" href="https://www.tripadvisor.com/Attraction_Review-g293734-d26818101-Reviews-Furious_Quad_Marrakech-Marrakech_Marrakech_Safi.html">
+            <a target="_blank"
+               href="https://www.tripadvisor.com/Attraction_Review-g293734-d26818101-Reviews-Furious_Quad_Marrakech-Marrakech_Marrakech_Safi.html">
                 <Tripadvisor/>
             </a>
         </div>
     </footer>
+    <div class="fixed bottom-0 right-0 p-4 z-50">
+       <a href="https://wa.me/message/JOP6K2MY3I54J1" target="_blank">
+           <div class="h-[50px] w-[50px] flex justify-center items-center bg-green-500 rounded-full ">
+               <WhatsApp/>
+           </div>
+       </a>
+    </div>
 </template>
 
 <script setup>
@@ -41,6 +50,7 @@ import Tiktok from "@/Icons/Tiktok.vue";
 import Facebook from "@/Icons/Facebook.vue";
 import Tripadvisor from "@/Icons/Tripadvisor.vue";
 import Airbnb from "@/Icons/Airbnb.vue";
+import WhatsApp from "@/Icons/WhatsApp.vue";
 
 const date = new Date();
 
