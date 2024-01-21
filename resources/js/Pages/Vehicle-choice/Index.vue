d710901c30dddf2154a01b9e10aaeedf77c07399
Dia <macbook@MacBook-Pro-de-macbook.local>
2024-01-21 22:51:46 +0100

    wip

--- resources/js/Pages/Vehicle-choice/Index.vue
+++ resources/js/Pages/Vehicle-choice/Index.vue
@@ -12,11 +12,11 @@
                 <img src="images/heroBgMobile.jpg" class="w-full h-full object-cover z-0 md:hidden block"
                      alt="quad and motocross in marrakech">
             </div>
-            <div class="h-screen  w-full flex flex-col gap-8 justify-center items-center absolute top-0 z-20">
+            <div class="h-screen  w-full flex flex-col lg:gap-8 gap-4 justify-center items-center absolute top-0 z-20">
                 <h1 class="lg:text-2xl font-bold text-white">Choose Vehicle</h1>
                 <div class="grid lg:grid-cols-3 gap-4 ">
                     <Link :href="route('buggy')"
-                          class="cursor-pointer flex flex-col justify-center items-center  border border-neutral-600 lg:h-44 lg:w-72 h-32 w-52 transition-all duration-300"
+                          class="cursor-pointer flex flex-col justify-center items-center  border border-neutral-600 lg:h-44 lg:w-72 h-24 w-52 transition-all duration-300"
                           :class="buggyHover?'bg-[#c10c10]':'bg-transparent'"
                           @mouseenter="buggyHover=!buggyHover"
                           @mouseleave="buggyHover=!buggyHover">
@@ -25,7 +25,7 @@
                     </Link>
 
                     <Link :href="route('quad')"
-                          class="cursor-pointer flex flex-col justify-center items-center  border border-neutral-600 lg:h-44 lg:w-72 h-32 w-52 transition-all duration-300"
+                          class="cursor-pointer flex flex-col justify-center items-center  border border-neutral-600 lg:h-44 lg:w-72 h-24 w-52 transition-all duration-300"
                           :class="quadHover?'bg-[#c10c10]':'bg-transparent'"
                           @mouseenter="quadHover=!quadHover"
                           @mouseleave="quadHover=!quadHover">
@@ -34,7 +34,7 @@
                     </Link>
 
                     <Link :href="route('motocross')"
-                          class="cursor-pointer flex flex-col justify-center items-center  border border-neutral-600 lg:h-44 lg:w-72 h-32 w-52 transition-all duration-300"
+                          class="cursor-pointer flex flex-col justify-center items-center  border border-neutral-600 lg:h-44 lg:w-72 h-24 w-52 transition-all duration-300"
                           :class="motocrossHover?'bg-[#c10c10]':'bg-transparent'"
                           @mouseenter="motocrossHover=!motocrossHover"
                           @mouseleave="motocrossHover=!motocrossHover">
