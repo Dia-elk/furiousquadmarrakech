5efe9f563beaa75f31855c0ac699baa04c522126
Dia <macbook@MacBook-Pro-de-macbook.local>
2024-03-07 15:02:05 +0100

    wip

--- resources/js/Pages/Paragliding/Index.vue
+++ resources/js/Pages/Paragliding/Index.vue
@@ -1,4 +1,7 @@
 <template>
+    <Head>
+        <title>Paragliding Marrakech</title>
+    </Head>
  <MainLayout>
      <VehicleHeroSection title="Paragliding Marrakech" description="We're coming Soon !" image="/images/paragliding.jpg" mobile-image="/images/paragliding.jpg"/>
      <ContactSection/>
@@ -10,4 +13,5 @@
 import MainLayout from "@/Layouts/MainLayout.vue";
 import VehicleHeroSection from "@/Components/sections/VehicleHeroSection.vue";
 import ContactSection from "@/Components/sections/ContactSection.vue";
+import {Head} from "@inertiajs/vue3";
 </script>
