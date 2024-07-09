450cc6b9e2d6093aa5ec1496689305cc49e03d89
Dia <macbook@MacBook-Pro-de-macbook.local>
2024-07-09 21:27:35 +0100

    wip

--- resources/js/Pages/CanAm/Index.vue
+++ resources/js/Pages/CanAm/Index.vue
@@ -2,13 +2,13 @@
     <Head>
         <link rel="canonical" href="https://furiousquadmarrakech.com/buggy-marrakech" />
         <title>Can-Am Marrakech | Exciting Off-Road Adventures in Morocco</title>
-        <meta name="description" content="Experience the thrill of off-road adventure with our Buggy Marrakech tours! Explore the scenic beauty of Morocco on our guided buggy tours.">
-        <meta name="keywords" content="Buggy Marrakech, Marrakech off-road tours, Buggy adventures Morocco, Off-roading experiences, Marrakech excursions">
+        <meta name="description" content="Explore the thrill of Can-Am Adventures! Pilot our high-performance Can-Am vehicles across a variety of terrains and experience exhilarating escapades. Whether navigating through desert sands or winding mountain paths, every ride promises unmatched excitement.">
+        <meta name="keywords" content="Can-Am Marrakech, Marrakech off-road tours, Can-Am adventures Morocco, Off-roading experiences, Marrakech excursions">
     </Head>
     <VehicleLayout :title="'Can-Am Marrakech'"
                    :description="'Explore the thrill of Can-Am Adventures! Pilot our high-performance Can-Am vehicles across a variety of terrains and experience exhilarating escapades. Whether navigating through desert sands or winding mountain paths, every ride promises unmatched excitement.'"
                    :image="'images/heroCanAmBg.avif'"
-                   :mobile-image="'images/heroBuggyBgMobile.jpg'"
+                   :mobile-image="'images/heroCanAmBg.avif'"
     >
         <PackagesSection :packs="packs"  :title="'Can-AM Marrakech'"/>
     </VehicleLayout>
