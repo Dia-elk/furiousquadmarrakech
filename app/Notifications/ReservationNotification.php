4efebcd159c02a5b7413a694bb68066a81250562
Dia <macbook@MacBook-Pro-de-macbook.local>
2024-11-10 18:05:20 +0100

    wip

--- app/Notifications/ReservationNotification.php
+++ app/Notifications/ReservationNotification.php
@@ -40,6 +40,7 @@ class ReservationNotification extends Notification implements ShouldQueue
      */
     public function toSlack(object $notifiable): SlackMessage
     {
+        $source = $this->reservation->source->value===SourceEnum::FURIOUS_EN->value?'furiousquadmarrakech.com':'quad-amarrakech.com';
         return (new SlackMessage)
                     ->content(
                         '
@@ -48,7 +49,7 @@ class ReservationNotification extends Notification implements ShouldQueue
 🎉 New Reservation 🎉
 
 
-Source: '.$this->reservation->source->value.'.
+Source: '.$source.'.
 Email: '.$this->reservation->customer->email.'.
 Phone: '.$this->reservation->customer->phone_number.'.
 Country: '.$this->reservation->country.'.
