31ac77bd24b9dd5a22ef821fc003fcd732bb09c8
Dia <macbook@MacBook-Pro-de-macbook.local>
2024-11-11 14:00:42 +0100

    throttle

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('furious/{pack:slug}',[ApiReservationController::class,'getPack']);
-Route::post('furious/{pack:slug}/book',[ApiReservationController::class,'store']);
-Route::post('furious/contact',[ApiContactController::class,'store']);
-Route::get('furious/packs/quad',[ApiPackController::class,'quad']);
-Route::get('furious/packs/buggy',[ApiPackController::class,'buggy']);
-Route::get('furious/packs/motocross',[ApiPackController::class,'motocross']);
-Route::get('furious/packs/can-am',[ApiPackController::class,'canAm']);
+Route::group([
+    'middleware'=>'throttle:60,1'
+],function (){
+    Route::get('furious/{pack:slug}',[ApiReservationController::class,'getPack']);
+    Route::post('furious/{pack:slug}/book',[ApiReservationController::class,'store']);
+    Route::post('furious/contact',[ApiContactController::class,'store']);
+    Route::get('furious/packs/quad',[ApiPackController::class,'quad']);
+    Route::get('furious/packs/buggy',[ApiPackController::class,'buggy']);
+    Route::get('furious/packs/motocross',[ApiPackController::class,'motocross']);
+    Route::get('furious/packs/can-am',[ApiPackController::class,'canAm']);
+})
